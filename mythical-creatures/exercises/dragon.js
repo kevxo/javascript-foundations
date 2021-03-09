9bc273251cb747f2b9f8d6a1ca6b0d54c0584f5d
Kevin Cuadros <kev040798@gmail.com>
2021-03-09 18:10:30 -0500

    Complete dragon exercise

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,24 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.count = 0
+    this.hungry = true;
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.count += 1;
+    if (this.count < 3){
+      this.hungry;
+    } else {
+      this.hungry = false;
+    }
+  }
+
+}
+
+module.exports = Dragon;
