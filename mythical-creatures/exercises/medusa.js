861a86875df60d638c4de2905ed089d241ca847f
Kevin Cuadros <kev040798@gmail.com>
2021-04-02 20:21:50 -0400

    Finish medusa exercise

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,22 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim){
+
+    if (this.statues.length === 3){
+      var person = this.statues.shift();
+      this.statues.push(new Statue(victim.name));
+      return new Person(person.name, 'relieved');
+    } else {
+      this.statues.push(new Statue(victim.name));
+    }
+  }
+}
+
+module.exports = Medusa
