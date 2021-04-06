c8eef9f704990b8f1ae09a485f66a20b9fb47e33
Kevin Cuadros <kev040798@gmail.com>
2021-04-06 14:29:38 -0400

    Finish Fairy exercise

---
+++ mythical-creatures/exercises/fairy.js
@@ -0,0 +1,44 @@
+class Fairy {
+  constructor(name) {
+    this.name = name;
+    this.dust = 10;
+    this.clothes = {
+      dresses: ['Iris']
+    }
+    this.disposition = 'Good natured';
+    this.humanWards = [];
+  }
+
+  receiveBelief() {
+    this.dust += 1;
+  }
+
+  believe() {
+    this.dust += 10;
+  }
+
+  makeDresses(dresses) {
+    for (let i = 0; i < dresses.length; i++) {
+      this.clothes.dresses.push(dresses[i]);
+    }
+  }
+
+  becomeProvoked() {
+    this.disposition = 'Vengeful'
+  }
+
+  replaceInfant(infant){
+    if (this.disposition === 'Vengeful'){
+      infant.disposition = 'Malicious'
+      this.humanWards.push(infant);
+    } else {
+      return infant
+    }
+
+    if (this.humanWards.length === 3){
+      this.disposition = 'Good natured'
+    }
+  }
+}
+
+module.exports = Fairy
