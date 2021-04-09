3e8052bb7e9e6162e8a20d33a9e74a3100ace5f2
Kevin Cuadros <kev040798@gmail.com>
2021-04-08 20:09:34 -0400

    Finish golf exercises

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,35 @@
 class GolfCourse {
+  constructor(name, difficulty, openings = 0, features = []) {
+    this.name = name;
+    this.difficulty = difficulty;
+    this.openings = openings;
+    this.features = features;
+    this.currentlyPlaying = [];
+  }
 
+  checkInGroup(group){
+    var new_array = group.sort(function(a,b){
+      return a.handicap - b.handicap;
+    })
 
+    var pushCount = 0
+    for (var i = 0; i < new_array.length; i++){
+      if (this.currentlyPlaying.length <= this.openings){
+        this.currentlyPlaying.unshift(new_array[i].name);
+        pushCount++;
+      }
+    }
+
+    var x = this.openings - new_array.length
+    this.openings = (this.openings - pushCount);
+
+    if (x >= 0){
+      return 'You\'re checked in. Have fun!'
+    } else {
+      return 'Sorry, we are currently booked! Please come back later.'
+    }
+
+  }
 }
 
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,48 @@
 class Golfer {
+  constructor(data) {
+    this.name = data['name'];
+    this.handicap = data['handicap'];
+    this.frustration = 0;
+    this.confidence = 0;
+  }
 
+  calculateAvg(par) {
+    var avg = ((par + this.handicap) / 1)
+    return `I usually shoot a ${avg} on average.`
+  }
 
+  playRound(course){
+    if (course.difficulty === 'hard'){
+      this.frustration += 500
+    } else {
+      this.frustration += 100
+    }
+  }
+
+  hitTheRange() {
+    this.confidence += 10
+  }
+
+  simulatePractice(times, player){
+    (times * 10 ) + player.confidence
+  }
+
+  marvel(course) {
+    return `I love the ${course.features[0]} and ${course.features[1]} on this course!`
+  }
+
+  whatYaShoot(score){
+    if (score < 0) {
+      this.frustration = 0;
+      return 'I AM THE GREATEST GOLFER ALIVE!'
+    } else if (score === 0) {
+      this.frustration -= 10;
+      return 'Booyah!'
+    } else if (score > 0){
+      this.frustration += 20;
+      return 'Doh!'
+    }
+  }
 }
 
 module.exports = Golfer;
